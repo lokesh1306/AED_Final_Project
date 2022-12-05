4dd53e71107e46e625a0bc87c5d2c37e9c518c66
lokesh1306 <challagulla.l@northeastern.edu>
2022-12-05 15:18:31 -0500

    Initial Commit

---
+++ FinalProject/src/Business/ConfigureASystem.java
@@ -0,0 +1,32 @@
+package Business;
+
+import Business.Worker.Worker;
+import Business.Role.FCAdminRole;
+import Business.Role.SystemAdminRole;
+import Business.UserAccount.UserAccount;
+
+/**
+ *
+ * @author admin
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure(){
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        //Create a network
+        //create an enterprise
+        //initialize some organizations
+        //have some employees 
+        //create user account
+        
+        
+        Worker employee = system.getEmployeeDirectory().createEmployee("sysadmin");
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee, new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
